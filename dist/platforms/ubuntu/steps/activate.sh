46ed7ceaf994779e64e16c594f0cb71ef445f04f
Sébastien Aglaé <38996444+sebastienaglae@users.noreply.github.com>
2023-02-20 23:06:17 +0100

    Add activation

--- dist/platforms/ubuntu/steps/activate.sh
+++ dist/platforms/ubuntu/steps/activate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Bypassing activation :)"
+/opt/unity/Editor/Unity -batchmode -nographics -logfile /dev/stdout -manualLicenseFile /root/share/unity3d/Unity/Unity_lic.ulf -quit
